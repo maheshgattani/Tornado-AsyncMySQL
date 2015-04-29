c20c8da2119469ccd6e72d8ab8ba4c6516225482
mahesh gattani <maheshgattani@gmail.com>
2015-04-29 14:27:34 -0400

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -7,26 +7,14 @@ from adisp import process
 import json
 
 class Connect(object):
-  config = None
-  logger = None
-  conn = {'ro': None, 'rw': None}
-  cache = None
   adb = None
-  cache_prefix = 'sales_rules'
-
-  @classmethod
-  def set_config(cls,config,logger):
-    cls.config = config
-    cls.logger = logger
-    cls.adb = None
-    # cls._init_cache()
 
   @classmethod
   def _connect_mysql(cls, conn_type='ro'):
     prefix = '%s_' % conn_type
     if cls.adb is None:
       #print "Initializing adb"
-      cls.adb = adb.Database(driver="MySQLdb", database="magento_birchbox", user="mahesh", password="ohnohzahP0tahSho", host="localhost")
+      cls.adb = adb.Database(driver="MySQLdb", database="test_db", user="user", password="password", host="localhost")
 
   @classmethod
   def _get_adb(cls, conn_type='ro'):
@@ -38,22 +26,17 @@ class Connect(object):
 
 @process
 def fetch(sql, params, retry=False, conn_type='ro', callback=None):
-  #print "should initialize adb"
   adb = Connect._get_adb(conn_type)
-  #print "about to run run query"
   data = yield adb.runQuery(sql)
   if callback is not None:
-    #print "callback made"
     callback(data)
   
 class MainHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     def get(self):
-      fetch("select sleep(0.1); select count(*) from core_config_data", {}, callback = self.callback)
+      fetch("select count(*) from test_table", {}, callback = self.callback)
 
     def callback(self, results):
-      #print "Callback received"
-      #print results
       self.write(json.dumps(results))
       self.finish()
       
@@ -63,5 +46,5 @@ application = tornado.web.Application([
 ])
 
 if __name__ == "__main__":
-    application.listen(8889)
+    application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
